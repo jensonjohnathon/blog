bcc42031b3bec29cb1f7af647e1d1ccf3fb68125
johnathon <jenson.j.reggentin@hochschule-stralsund.de>
2025-02-15 15:36:17 +0100

    customization of basic components

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,17 +1,17 @@
-import TwitterIcon from '@/components/icons/TwitterIcon'
+import LinkedInIcon from '@/components/icons/LinkedInIcon'
 import GithubIcon from '@/components/icons/GithubIcon'
 
 // ADD YOUR SOCIAL NETWORKS HERE
 export const SOCIALNETWORKS = [
 	{
 		name: 'Github',
-		url: 'https://github.com/danielcgilibert/blog-template',
+		url: 'https://github.com/jensonjohnathon',
 		icon: GithubIcon
 	},
 
 	{
-		name: 'Twitter',
-		url: 'https://github.com/danielcgilibert/blog-template',
-		icon: TwitterIcon
+		name: 'LinkedIn',
+		url: 'https://www.linkedin.com/in/jenson-reggentin-895ab8343',
+		icon: LinkedInIcon
 	}
 ] as const
